95f22159101d2234e4098090b3c7cc8bb7d36d0d
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2025-12-24 09:30:44 +0100

    fixed importing errors

--- server/handlers/users.ts
+++ server/handlers/users.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
+
 
 export function getUsers(request: Request, response: Response) {
     response.send([])
+
 }
 
 
 export function getUserById(request: Request, response: Response) {
-    response.send({})
+   response.send({})
 }
