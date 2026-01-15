f9538702c8029dfc13201f56ef5d5d870a11dae6
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-01-15 19:57:36 +0900

    switching tabs completed

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -91,7 +91,7 @@ const Modal: React.FC<ModalProps> = ({
           onClick={toggleModal}
           className="h-screen inset-0 fixed z-4ß bg-black/60"
         ></div>
-        <div className="absolute text-center z-50 bg-gray-200 rounded-xl top-[50%] left-[50%] h-5/12 w-4xl -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute text-center z-50 bg-gray-200 rounded-xl top-[50%] left-[50%] h-6/12 w-4xl -translate-x-1/2 -translate-y-1/2">
           <div>
             <label>
               <input
@@ -122,6 +122,10 @@ const Modal: React.FC<ModalProps> = ({
               Offboarding
             </label>
             {selectedOption === "Onboarding" && (
+              <WorkerDataForm type={selectedOption} onSubmit={onSubmit} />
+            )}
+
+            {selectedOption === "Offboarding" && (
               <WorkerDataForm type={selectedOption} onSubmit={onSubmit} />
             )}
 
--- client/src/components/workerData.tsx
+++ client/src/components/workerData.tsx
@@ -9,6 +9,7 @@ type Inputs = {
   Eintrittsdatum: string;
   Adresse: string | number;
   Position: string;
+  Austrittsdatum?: string;
 };
 
 interface WorkerDataFormProps {
@@ -29,25 +30,29 @@ export const WorkerDataForm = ({ type, onSubmit }: WorkerDataFormProps) => {
       <form className="flex flex-col max-w-2xl justify-center gap-6 mx-auto">
         <div className="flex flex-row gap-6 mx-auto">
           <div className="flex flex-col gap-4 items-center focus:outline-none">
+            {/* this should always be shown */}
+            <label>Vorname</label>
             <input
               className="w-64 p-2 border-gray-300 border-2 rounded-xl"
               type="text"
               placeholder="Vorname"
               {...register("Vorname")}
             />
+            <label>Nachname</label>
             <input
               className="w-64 p-2 border-gray-300 border-2 rounded-xl"
               type="text"
               placeholder="Nachname"
               {...register("Nachname")}
             />
+            <label>Geburtsdatum</label>
             <input
               className="w-64 p-2 border-gray-300 border-2 rounded-xl"
               type="text"
-              defaultValue="test"
               placeholder="DD.MM.YYYY"
               {...register("Geburtsdatum")}
             />
+            <label>Adresse</label>
             <input
               className="w-64 p-2 border-gray-300 border-2 rounded-xl"
               type="text"
@@ -59,13 +64,32 @@ export const WorkerDataForm = ({ type, onSubmit }: WorkerDataFormProps) => {
 
           <div className="flex flex-col max-w-md gap-4">
             {/* <Input placeholder="name" /> */}
+            <label>Eintrittsdatum</label>
             <input
               className="w-64 p-2 border-gray-300 border-2 rounded-xl"
               type="text"
-              defaultValue="test"
               placeholder="DD.MM.YYYY"
               {...register("Eintrittsdatum")}
             />
+
+            {/* start of component that should only show in offboarding*/}
+
+            {type === "Offboarding" ? (
+              <>
+                <label>Austrittsdatunm</label>
+                <input
+                  className="w-64 p-2 border-gray-300 border-2 rounded-xl"
+                  type="text"
+                  placeholder="DD.MM.YYYY"
+                  {...register("Austrittsdatum")}
+                />
+              </>
+            ) : (
+              ""
+            )}
+
+            {/* end of component that should only show in offboarding */}
+            <label>Position</label>
             <input
               className="w-64 p-2 border-gray-300 border-2 rounded-xl"
               type="text"
