77c2b108ce231ed4a5447820486f8c150de7dc2a
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-01-23 12:07:23 +0100

    adds query client provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,10 @@ import { router } from "./router";
 import "./index.css";
 import "./styles/theme.css";
 import { RouterProvider } from "@tanstack/react-router";
-
-const queryClient = new QueryClient();
+import queryClient from "./config/query.client";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
