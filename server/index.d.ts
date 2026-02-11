7b8bc6d1429c742543e83928fc8020fd97e45e3f
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-02-11 20:10:29 +0100

    trying to fix build

--- server/index.d.ts
+++ server/index.d.ts
@@ -1,12 +1,11 @@
-import { Session, User } from "./generated/prisma/client";
-
+import type { Session, User } from "shared_prisma_types/prisma";
 declare global {
-  namespace Express {
-    interface Request {
-      userId: User["id"];
-      sessionId: Session["id"];
+    namespace Express {
+        interface Request {
+            userId: User["id"];
+            sessionId: Session["id"];
+        }
     }
-  }
 }
 
 export {};
