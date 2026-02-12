4ee9e0f8ca546181b34adefda2e4635afdadcd75
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-02-12 08:52:00 +0100

    Fixes routing

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,7 +2,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Dashboard from "@/features/Dashboard";
 import { LoginComponent } from "@/features/Login";
+import BSBHomePage from "@/features/BSBHomePage";
 
 export const Route = createFileRoute("/")({
-    component: LoginComponent,
+    component: BSBHomePage,
 });
--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -4,29 +4,29 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { LoginComponent } from "@/features/Login";
 
 export const Route = createFileRoute("/login")({
-  component: LoginForm,
+    component: LoginForm,
 });
 
 enum GenderEnum {
-  female = "female",
-  male = "male",
-  other = "other",
+    female = "female",
+    male = "male",
+    other = "other",
 }
 
 interface IFormInput {
-  firstName: string;
-  gender: GenderEnum;
+    firstName: string;
+    gender: GenderEnum;
 }
 
 export default function LoginForm() {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+    const { register, handleSubmit } = useForm<IFormInput>();
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginComponent />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <LoginComponent />
+            </div>
+        </div>
+    );
 }
