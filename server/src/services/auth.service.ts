4393daa378fda437c2c9363835afd232b3613f5d
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-05 19:49:45 +0100

    cleanup on server

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -59,8 +59,7 @@ export const createAccount = async (data: createAccountParams) => {
             password: hashedpassword,
             vorname: data.firstName,
             nachname: data.lastName,
-            cloud_url:
-                "https://bsb-file-storage.s3.eu-north-1.amazonaws.com/upload/profilepic/cmknti1f800028tmmhf5u5627/1771746653414-",
+            cloud_url: "",
         },
         omit: {
             password: true,
