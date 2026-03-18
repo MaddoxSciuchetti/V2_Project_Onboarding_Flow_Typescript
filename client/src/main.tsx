c68c02a02a6339de565f9c49aff7cf007f53358c
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-18 07:41:36 +0100

    fix: remove double from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,10 @@ import './index.css';
 
 import AppRouter from './AppRouter';
 import queryClient from './config/query.client';
-import { ThemeProvider } from './context/theme-provider/ThemeProvider';
 import './styles/theme.css';
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <AppRouter />
-    </ThemeProvider>
+    <AppRouter />
   </QueryClientProvider>
 );
