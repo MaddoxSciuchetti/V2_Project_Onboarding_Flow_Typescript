952b56f7f711aac1ad8fcf079a1955e30e35e5dc
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-04-13 17:47:40 +0200

    update type for User

--- client/src/features/user-profile/types/auth.type.ts
+++ client/src/features/user-profile/types/auth.type.ts
@@ -1,13 +1,18 @@
 export type User = {
   id: string;
   email: string;
-  verified: boolean;
-  user_permission: 'CHEF' | 'MITARBEITER';
-  createdAt: Date;
-  updatedAt: Date;
-  vorname: string;
-  nachname: string;
-  cloud_url: string;
+  verified?: boolean;
+  isEmailVerified?: boolean;
+  user_permission?: 'CHEF' | 'MITARBEITER';
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  vorname?: string;
+  nachname?: string;
+  cloud_url?: string;
+  firstName?: string;
+  lastName?: string;
+  displayName?: string;
+  avatarUrl?: string;
   presignedUrl?: string;
 };
 
