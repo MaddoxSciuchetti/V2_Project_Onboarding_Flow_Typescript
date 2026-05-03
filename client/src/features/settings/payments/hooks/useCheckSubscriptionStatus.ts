f347fe44a79a0938a9af99546f4e7a4dc07b088b
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-05-03 08:19:40 +0200

    refactor billing hooks into two seperate hooks

---
+++ client/src/features/settings/payments/hooks/useCheckSubscriptionStatus.ts
@@ -0,0 +1,21 @@
+import queryClient from '@/config/query.client';
+import { useEffect } from 'react';
+import { toast } from 'sonner';
+import { billingQueries } from '../../billing.queries';
+
+export function useCheckSubscriptionStatus() {
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get('success')) {
+      toast.success('Abo erfolgreich aktiviert.');
+      void queryClient.invalidateQueries({
+        queryKey: billingQueries.subscription().queryKey,
+      });
+      window.history.replaceState({}, '', '/settings/payments');
+    } else if (query.get('canceled')) {
+      toast.info('Bezahlvorgang abgebrochen.');
+      window.history.replaceState({}, '', '/settings/payments');
+    }
+  }, []);
+}
